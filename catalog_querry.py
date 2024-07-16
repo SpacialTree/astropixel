9c1027009b8e6f6c2dc7d557a81d3456a410a094
Savannah Gramze <savannahgramze@gmail.com>
2024-07-16 13:30:11 -0500

    added image query

--- catalog_querry.py
+++ catalog_querry.py
@@ -22,6 +22,17 @@ def get_sdss_catalog_rect(ra, dec, width=1.0*u.deg, height=1.0*u.deg):
     # query the SDSS catalog
     xid = SDSS.query_region(c, width=width, height=height)
 
+def get_sdss_image(ra, dec, radius=1.0):
+    """
+    Function to get the SDSS image around a given RA and DEC
+    """
+    # create a SkyCoord object
+    c = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg), frame='icrs')
+    # get the SDSS image
+    xid = SDSS.query_region(c, radius=radius*u.deg)
+    img = SDSS.get_images(matches=xid)
+    return img
+
 def get_random_coordinates():
     """
     Function to generate random coordinates
