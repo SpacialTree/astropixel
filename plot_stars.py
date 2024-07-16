f838c0d4df6a1476e663268606843737a26e92d4
Savannah Gramze <savannahgramze@gmail.com>
2024-07-16 16:46:23 -0500

    working:wq

--- plot_stars.py
+++ plot_stars.py
@@ -7,19 +7,25 @@ from astropy.wcs import WCS
 
 
 def get_wcs(coord, size=(1000, 1000)):
+    """ 
+    Function to get WCS
+    """
     wcs = WCS(naxis=2)
     wcs.wcs.crpix = [size[0]/2, size[1]/2]  # Set the reference pixel to the center of the image
     wcs.wcs.crval = [coord.ra.deg, coord.dec.deg]  # Set the reference value to the given coordinates
-    wcs.wcs.cdelt = np.array([-0.001, 0.001])  # Set the pixel scale (assuming 0.001 degrees per pixel)
+    wcs.wcs.cdelt = np.array([-0.00005, 0.00005])  # Set the pixel scale (assuming 0.00001 degrees per pixel)
+    wcs.wcs.ctype = ["RA---TAN", "DEC--TAN"]  # Set the coordinate type to RA/DEC
+
     return wcs
 
-def plot_field(coord, cat, wcs):
+def plot_field(coord, cat, wcs, ax=None):
     """
     Function to plot field
     """
 
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection=wcs)
+    if ax is None:
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(111, projection=wcs)
 
     ax.scatter(cat['RAJ2000'], cat['DEJ2000'], s=50, marker='*', color='k')
     ax.scatter(coord.ra.deg, coord.dec.deg, s=100, edgecolor='r', facecolor='none')
@@ -29,16 +35,29 @@ def plot_field(coord, cat, wcs):
     ax.set_ylabel('Declination')
 
     plt.tight_layout()
-    plt.show()
+    return ax
 
-def plot_random_field(size=(1000, 1000)):
+def plot_coord(coord, size=(1000, 1000), ax=None):
+    """
+    Function to plot field at coord
+    """
+    wcs = get_wcs(coord, size=size)
+    cat = catalog_querry.get_2mass_catalog(coord, 1*u.arcmin)
+
+    return plot_field(coord, cat, wcs, ax=ax)
+
+def plot_random_field(size=(1000, 1000), ax=None):
     """
     Function to plot random field
     """
 
     coord = catalog_querry.get_random_coordinates()
     wcs = get_wcs(coord)
-    cat = catalog_querry.get_2mass_catalog(coord, 1*u.arcmin)
+    try: 
+        cat = catalog_querry.get_2mass_catalog(coord, 1*u.arcmin)
+    except:
+        print('No catalog found. Trying again...')
+
     i = 0
     while len(cat) < 10:
         coord = catalog_querry.get_random_coordinates()
@@ -49,7 +68,7 @@ def plot_random_field(size=(1000, 1000)):
             print("No catalog found")
             break
 
-    plot_field(coord, cat, wcs)
+    plot_field(coord, cat, wcs, ax=ax)
 
 def main():
     plot_random_field()
