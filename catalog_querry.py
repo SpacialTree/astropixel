9adc0146adcfacbd1d30b28bf7e3dd2bd0bba684
Savannah Gramze <savannahgramze@gmail.com>
2024-07-16 13:28:22 -0500

    fndg

--- catalog_querry.py
+++ catalog_querry.py
@@ -4,14 +4,23 @@ from astroquery.sdss import SDSS
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_sdss_catalog(ra, dec, radius=1.0):
+def get_sdss_catalog_circ(ra, dec, radius=1.0*u.deg):
     """
     Function to get the SDSS catalog around a given RA and DEC
     """
     # create a SkyCoord object
     c = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg), frame='icrs')
     # query the SDSS catalog
-    xid = SDSS.query_region(c, radius=radius*u.deg)
+    xid = SDSS.query_region(c, radius=radius)
+
+def get_sdss_catalog_rect(ra, dec, width=1.0*u.deg, height=1.0*u.deg):
+    """
+    Function to get the SDSS catalog around a given RA and DEC
+    """
+    # create a SkyCoord object
+    c = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg), frame='icrs')
+    # query the SDSS catalog
+    xid = SDSS.query_region(c, width=width, height=height)
 
 def get_random_coordinates():
     """
