eb6e606df9477cb8496e104727c8781360c099a4
Savannah Gramze <savannahgramze@gmail.com>
2024-07-17 16:29:11 -0500

    added doc strings to functions

--- astropixel/plot_stars.py
+++ astropixel/plot_stars.py
@@ -12,7 +12,21 @@ import make_star
 
 
 class StarPlotter:
+    """ 
+    Class to plot stars
+    """
+
     def __init__(self, coord, size=(1000, 1000), radius=1*u.arcmin):
+        """ 
+        Initialize the class
+
+        Args:
+            coord (SkyCoord): Coordinates of the center of the field.
+            size (tuple): Size of the field in pixels.
+            radius (Quantity): Radius of the field.
+
+        """
+
         self.coord = coord
         self.size = size
         self.size_scale = size[0]/10
@@ -25,6 +39,9 @@ class StarPlotter:
     def get_wcs(self):
         """ 
         Function to get WCS
+
+        Returns:
+            WCS: WCS object.
         """
         wcs = WCS(naxis=2)
         wcs.wcs.crpix = [self.size[0]/2, self.size[1]/2]  # Set the reference pixel to the center of the image
@@ -37,6 +54,12 @@ class StarPlotter:
     def plot_crosshair(ax):
         """
         Function to plot crosshair
+
+        Args:
+            ax (Axes): Axes object.
+
+        Returns:
+            ax (Axes): Axes object.
         """
 
         ax.scatter(self.coord.ra.deg, self.coord.dec.deg, s=100, edgecolor='r', facecolor='none')
@@ -46,6 +69,13 @@ class StarPlotter:
     def plot_scatter_field(self, labels=False, ax=None):
         """
         Function to plot field with scatter
+
+        Args:
+            labels (bool): Show labels.
+            ax (Axes): Axes object.
+
+        Returns:
+            ax (Axes): Axes object.
         """
 
         if ax is None:
@@ -68,6 +98,12 @@ class StarPlotter:
     def get_cross_psf_field_image(self, band='Kmag'):
         """
         Function to get cross PSF field image
+
+        Args:
+            band (str): Band to use. Must match the column name in the catalog self.cat
+
+        Returns:
+            np.ndarray: Image of the field with cross PS
         """
         star = make_star.GaussianCrossPSF(amplitude=1)
         psf = np.zeros((self.size[0], self.size[1]))
@@ -83,6 +119,13 @@ class StarPlotter:
     def plot_cross_psf_field(self, labels=False, ax=None):
         """
         Function to plot cross PSF field with imshow
+
+        Args:
+            labels (bool): Show labels.
+            ax (Axes): Axes object.
+
+        Returns:
+            ax (Axes): Axes object.
         """
         if ax is None:
             fig = plt.figure(figsize=(10, 8))
@@ -105,6 +148,13 @@ class StarPlotter:
     def plot_cross_psf_field_rgb(self, labels=False, ax=None):
         """
         Function to plot cross PSF field with imshow
+
+        Args:
+            labels (bool): Show labels.
+            ax (Axes): Axes object.
+
+        Returns:
+            ax (Axes): Axes object.
         """
         if ax is None:
             fig = plt.figure(figsize=(10, 8))
@@ -132,6 +182,14 @@ class StarPlotter:
 def make_rgb_scaled_image(psf_R, psf_G, psf_B):
     """
     Function to make RGB image
+
+    Args:
+        psf_R (np.ndarray): Image of the field for the R color.
+        psf_G (np.ndarray): Image of the field for the G color.
+        psf_B (np.ndarray): Image of the field for the B color.
+
+    Returns:
+        np.ndarray: RGB image.
     """
     rgb = np.array(
         [
@@ -152,13 +210,31 @@ def make_rgb_scaled_image(psf_R, psf_G, psf_B):
 
 def plot_random_scatter_field(size=(1000, 1000), radius=1*u.arcmin, ax=None):
     """
-    Function to plot random field
+    Function to plot random field.
+
+    Args:
+        size (tuple): Size of the field in pixels.
+        radius (Quantity): Radius of the field.
+        ax (Axes): Axes object.
+
+    Returns:
+        ax (Axes): Axes object.
     """
     coord = catalog_querry.get_random_coordinates_gal()
     field = StarPlotter(coord, radius=radius, size=size)
     field.plot_field(ax=ax)
 
 def magnitude_to_luminosity(magnitude):
+    """ 
+    Function to convert magnitude to luminosity
+
+    Args:
+        magnitude (float): Magnitude of the star.
+
+    Returns:
+        float: Luminosity of the star
+
+    """
     # Convert magnitude to flux
     flux = 10**(-0.4 * magnitude)
     
@@ -168,14 +244,43 @@ def magnitude_to_luminosity(magnitude):
     return luminosity
 
 def scale_the_magnitude(magnitude, scale=5):
+    """ 
+    Function to scale the magnitude
+
+    Args:
+        magnitude (float): Magnitude of the star.
+        scale (float): Scale factor.
+
+    Returns:
+        float: Scaled magnitude.
+    """
     lum = magnitude_to_luminosity(magnitude)
 
     return lum**(1/scale)
 
 def average_magnitude(c):
+    """
+    Function to calculate the average magnitude of a star
+    
+    Args:
+        c (Table): Table of stars.
+
+    Returns:
+        float: Average magnitude of the star.
+    """
+
     return np.mean([c['Jmag'], c['Hmag'], c['Kmag']])
 
 def example_plot_scatter_field(ax=None):
+    """
+    Example to plot scatter field
+
+    Args:
+        ax (Axes): Axes object.
+
+    Returns:
+        ax (Axes): Axes object.
+    """
     coord = SkyCoord.from_name('Barnard\'s Star')
     field = StarPlotter(coord, size=(1000, 1000), radius=1*u.arcmin)
 
@@ -187,6 +292,16 @@ def example_plot_scatter_field(ax=None):
     plt.show()
 
 def example_plot_cross_psf_field(ax=None):
+    """ 
+    Example to plot cross PSF field
+
+    Args:
+        ax (Axes): Axes object.
+
+    Returns:
+        ax (Axes): Axes object.
+    """
+
     coord = SkyCoord.from_name('Barnard\'s Star')
     field = StarPlotter(coord, size=(1000, 1000), radius=1*u.arcmin)
 
@@ -198,6 +313,9 @@ def example_plot_cross_psf_field(ax=None):
     plt.show()
 
 def example_plot_cross_psf_field_rgb():
+    """
+    Example to plot cross PSF field RGB
+    """
     coord = SkyCoord.from_name('Barnard\'s Star')
     field = StarPlotter(coord, size=(50, 50), radius=1*u.arcmin)
 
