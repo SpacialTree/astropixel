8595b64ffe2692251c71192787fd3f15df9d1729
Savannah Gramze <savannahgramze@gmail.com>
2024-07-18 14:23:42 -0500

    fixing imports

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,9 @@
 import pytest
 import numpy as np
 import astropixel
+from astropixel import catalog_querry
+from astropixel import plot_stars
+from astropixel import make_star
 from astropixel.make_star import GaussianCrossPSF
 from astropy import units as u
 from astropy.coordinates import SkyCoord
@@ -14,11 +17,11 @@ def test_generate_cross_psf():
     background_factor = 0.1
     size = (300, 200)
     psf_cross = psf.generate_cross_psf(x_center, y_center, stddev, background_factor, size=size)
-    assert psf_cross.shape == size.T
+    assert psf_cross.shape == size
     assert np.max(psf_cross) == 1
     assert np.min(psf_cross) == 0
     assert np.argmax(psf_cross) == (150, 100)
 
 if __name__ == "__main__":
-    astropixel.catalog_querry.get_2mass_catalog(SkyCoord(ra=0, dec=0, unit='deg'), 1*u.arcmin)
+    catalog_querry.get_2mass_catalog(SkyCoord(ra=0, dec=0, unit='deg'), 1*u.arcmin)
     test_generate_cross_psf()
